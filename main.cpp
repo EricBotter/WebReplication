c847d77fa06882d61632f6d6af81657c02a9956d
Eric Botter <eric.botter@usi.ch>
2016-02-19 19:44:51 +0100

    POSIX socket stub

--- main.cpp
+++ main.cpp
@@ -1,8 +1,43 @@
-#include <iostream>
+#include <sys/socket.h>
+#include <sys/unistd.h>
+#include <netinet/in.h>
+#include "Log.h"
 
 using namespace std;
 
+#define SOCKET_PORT 8000
+
 int main() {
-	cout << "Hello, World!" << endl;
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		Log::f("Unable to open socket");
+		return 1;
+	}
+
+	sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(SOCKET_PORT);
+
+	if (bind(sockfd, (sockaddr*)&server, sizeof(server)) < 0) {
+		Log::f("Unable to bind socket");
+		return 2;
+	}
+
+	listen(sockfd, 5);
+	Log::d("Server started. Waiting for connection...");
+
+	int newsockfd, c = sizeof(sockaddr_in);
+	sockaddr_in client;
+	newsockfd = accept(sockfd, (sockaddr*)&client, &c);
+	if (newsockfd < 0) {
+		Log::f("Unable to accept incoming connection");
+		return 2;
+	}
+
+	//use send and recv to send/receive data.
+
+	close(sockfd);
+
 	return 0;
 }
